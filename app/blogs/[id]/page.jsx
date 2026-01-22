56a75a6f282648149239cac58ca6032b9bc93bc2
subishga06 <subishgavs@gmail.com>
2026-01-22 12:39:24 +0530

    Rename all page components to uppercase Page for React hooks

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -1,62 +1,65 @@
-'use client'
-import { assets, blog_data } from '@/Assets/assets';
-import Footer from '@/Components/Footer';
-import axios from 'axios';
-import Image from 'next/image';
-import Link from 'next/link';
-import React, { useEffect, useState } from 'react'
+import { ConnectDB } from "@/lib/config/db"
+import BlogModel from "@/lib/models/BlogModel";
+const { NextResponse } = require("next/server")
+import { writeFile } from 'fs/promises'
+const fs = require('fs')
 
-const Page = ({ params }) => {
-
-  const [data, setData] = useState(null);
-
-  const fetchBlogData = async () => {
-    const response = await axios.get('/api/blog', {
-      params: {
-        id: params.id
-      }
-    })
-    setData(response.data);
-  }
-
-  useEffect(() => {
-    fetchBlogData();
-  }, [])
-
-  return (data ? <>
-    <div className='bg-gray-200 py-5 px-5 md:px-12 lg:px-28'>
-      <div className='flex justify-between items-center'>
-        <Link href='/'>
-          <Image src={assets.logo} width={180} alt='' className='w-[130px] sm:w-auto' />
-        </Link>
-        <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000000]'>
-          Get started <Image src={assets.arrow} alt='' />
-        </button>
-      </div>
-      <div className='text-center my-24'>
-        <h1 className='text-2xl sm:text-5xl font-semibold max-w-[700px] mx-auto'>{data.title}</h1>
-        <Image className='mx-auto mt-6 border border-white rounded-full' src={data.authorImg} width={60} height={60} alt='' />
-        <p className='mt-1 pb-2 text-lg max-w-[740px] mx-auto'>{data.author}</p>
-      </div>
-    </div>
-    <div className='mx-5 max-w-[800px] md:mx-auto mt-[-100px] mb-10'>
-      <Image className='border-4 border-white' src={data.image} width={800} height={480} alt='' />
-      
-      <div className='blog-content' dangerouslySetInnerHTML={{__html:data.description}}>
-        
-      </div>
-      <div className='my-24'>
-        <p className='text-black font font-semibold my-4'>Share this article on social media</p>
-        <div className='flex'>
-          <Image src={assets.facebook_icon} width={50} alt='' />
-          <Image src={assets.twitter_icon} width={50} alt='' />
-          <Image src={assets.googleplus_icon} width={50} alt='' />
-        </div>
-      </div>
-    </div>
-    <Footer />
-  </> : <></>
-  )
+const LoadDB = async () => {
+  await ConnectDB();
 }
 
-export default Page
+LoadDB();
+
+
+// API Endpoint to get all blogs
+export async function GET(request) {
+
+  const blogId = request.nextUrl.searchParams.get("id");
+  if (blogId) {
+    const blog = await BlogModel.findById(blogId);
+    return NextResponse.json(blog);
+  }
+  else {
+    const blogs = await BlogModel.find({});
+    return NextResponse.json({ blogs })
+  }
+}
+
+
+// API Endpoint For Uploading Blogs
+export async function POST(request) {
+
+  const formData = await request.formData();
+  const timestamp = Date.now();
+
+  const image = formData.get('image');
+  const imageByteData = await image.arrayBuffer();
+  const buffer = Buffer.from(imageByteData);
+  const path = `./public/${timestamp}_${image.name}`;
+  await writeFile(path, buffer);
+  const imgUrl = `/${timestamp}_${image.name}`;
+
+  const blogData = {
+    title: `${formData.get('title')}`,
+    description: `${formData.get('description')}`,
+    category: `${formData.get('category')}`,
+    author: `${formData.get('author')}`,
+    image: `${imgUrl}`,
+    authorImg: `${formData.get('authorImg')}`
+  }
+
+  await BlogModel.create(blogData);
+  console.log("Blog Saved");
+
+  return NextResponse.json({ success: true, msg: "Blog Added" })
+}
+
+// Creating API Endpoint to delete Blog
+
+export async function DELETE(request) {
+  const id = await request.nextUrl.searchParams.get('id');
+  const blog = await BlogModel.findById(id);
+  fs.unlink(`./public${blog.image}`, () => { });
+  await BlogModel.findByIdAndDelete(id);
+  return NextResponse.json({ msg: "Blog Deleted" });
+}
