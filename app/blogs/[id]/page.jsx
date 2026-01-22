b08ba8d3453860ebd44832c4e6d92ae95b498fa6
subishga06 <subishgavs@gmail.com>
2026-01-22 12:24:26 +0530

    Fix page component names to uppercase for production build

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
 
-const page = ({ params }) => {
+const Page = ({ params }) => {
 
   const [data, setData] = useState(null);
 
@@ -59,4 +59,4 @@ const page = ({ params }) => {
   )
 }
 
-export default page
+export default Page
